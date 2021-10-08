9a27fca03b291036895236200f7e3f72e0d485ad
levinliu <levinmhliu@gmail.com>
2021-10-09 00:41:30 +0800

    check in project codes

---
+++ README.rst
@@ -0,0 +1,31 @@
+
+renxianqi
+=========
+
+A small tool to check the any absentee.
+
+By comparing the left text pane with right text pane and it sort out the missing part.
+
+人贤齐，一个小公具用来查询出席活动的人数是否到齐了。
+
+通过比对左边跟右边的内容，计算出左边还有哪些人员没有出席/出现在右边面板。
+
+Package hosted on [`pypi`_] and powered by [`pypi-seed`_]
+
+Installation / 安装
+--------------------------
+
+::
+
+    pip install renxianqi
+
+
+More Info 
+--------------------------
+
+Author `levin`_
+
+.. _`pypi`: https://pypi.org/
+.. _`levin`: https://github.com/levinliu
+.. _`pypi-seed`: https://pypi.org/project/pypi-seed/
+
---
+++ setup.py
@@ -0,0 +1,44 @@
+import os
+from distutils.core import setup
+from setuptools import find_packages
+
+from renxianqi.setting import NAME,VERSION
+
+README_RST = os.path.join(os.getcwd(), "README.rst")
+with open(README_RST, 'r') as rfile:
+    LONG_DESCRIPTION = rfile.read()
+
+
+setup(name=NAME,  # 包名
+      version=VERSION,  # 版本号
+      description='A small tool to check the absentee',
+      long_description=LONG_DESCRIPTION if LONG_DESCRIPTION else "Powered by py4u (LeiXueWei). This tool is very useful to check the absentee for any activities!",
+      author='levin',
+      author_email='991219092@qq.com',
+      url='https://blog.csdn.net/geeklevin',
+      install_requires=["pypinyin","pyperclip"],
+      license='Apache License 2.0',
+      packages=find_packages(),
+      platforms=["all"],
+      entry_points={
+          'console_scripts': [
+              'rxq = renxianqi.main:main',
+              'renxianqi = renxianqi.main:main',
+          ]
+      },
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Operating System :: OS Independent',
+          'Natural Language :: Chinese (Simplified)',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3',
+          'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Topic :: Software Development :: Libraries'
+      ],
+      )
